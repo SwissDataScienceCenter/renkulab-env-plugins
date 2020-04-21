094e9b3c183043135cc21fc93b02a4e16b69aeea
Emma Jablonski <e.jablonski@gmail.com>
2020-04-21 11:52:10 +0200

    build: add docs links to renku menu

--- renkulab_menu_extension/src/index.ts
+++ renkulab_menu_extension/src/index.ts
@@ -24,8 +24,8 @@ const extension: JupyterFrontEndPlugin<void> = {
     console.log('Renku Extension Activation!')
     const { commands } = app;
 
-    // Add a command to open renkulab.io
-    const back_to_renkulab = 'renku-tools:back-to-renkulab';
+    // 1. Add a command to open renkulab.io
+    const back_to_renkulab = 'renku-tools:back_to_renkulab';
     commands.addCommand(back_to_renkulab, {
       label: 'Open renkulab.io',
       caption: 'Execute renku-tools:back-to-renkulab Command',
@@ -38,7 +38,33 @@ const extension: JupyterFrontEndPlugin<void> = {
       }
     });
 
-    // Add the command to the command palette
+    const renkulab_docs = 'renku-tools:renkulab-docs';
+    commands.addCommand(renkulab_docs, {
+      label: 'Open Renkulab (platform) docs',
+      caption: 'Execute renku-tools:renkulab-docs Command',
+      execute: (args: any) => {
+
+        console.log(
+          `renku-tools:renkulab-docs has been called ${args['origin']}.`
+        );
+        window.open("https://renku.readthedocs.io/en/latest/")
+      }
+    });
+
+    const renku_python_docs = 'renku-tools:renku-python-docs';
+    commands.addCommand(renku_python_docs, {
+      label: 'Open renku (command-line interface) docs',
+      caption: 'Execute renku-tools:renku-python-docs Command',
+      execute: (args: any) => {
+
+        console.log(
+          `renku-tools:renku-python-docs has been called ${args['origin']}.`
+        );
+        window.open("https://renku-python.readthedocs.io/en/latest/")
+      }
+    });
+
+    // 2. Add the command to the command palette
     const category = 'Renku Tools';
     palette.addItem({
       command:back_to_renkulab,
@@ -46,14 +72,30 @@ const extension: JupyterFrontEndPlugin<void> = {
       args: { origin: 'from the palette' }
     });
 
+    palette.addItem({
+      command:renkulab_docs,
+      category,
+      args: { origin: 'from the palette' }
+    });
 
-    // Create a menu
+    palette.addItem({
+      command:renku_python_docs,
+      category,
+      args: { origin: 'from the palette' }
+    });
+
+
+    // 3. Create a menu (just once)
     const renkuMenu: Menu = new Menu({ commands });
     renkuMenu.title.label = 'Renku Tools Menu';
     mainMenu.addMenu(renkuMenu, { rank: 80 });
 
-    // Add the command to the menu
+    // 4. Add the command to the menu
     renkuMenu.addItem({ command:back_to_renkulab, args: { origin: 'from the menu' } });
+
+    renkuMenu.addItem({ command:renkulab_docs, args: { origin: 'from the menu' } });
+
+    renkuMenu.addItem({ command:renku_python_docs, args: { origin: 'from the menu' } });
 
   }
 };
