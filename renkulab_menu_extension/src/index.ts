5daaa4a34cae4ff8e156904cc4ed37bd9aba8c24
Emma Jablonski <e.jablonski@gmail.com>
2020-04-21 09:37:39 +0200

    build: working copy of https://github.com/jupyterlab/extension-examples/tree/master/main-menu

--- renkulab_menu_extension/src/index.ts
+++ renkulab_menu_extension/src/index.ts
@@ -3,14 +3,57 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { IMainMenu } from '@jupyterlab/mainmenu';
+
+import { Menu } from '@lumino/widgets';
+
+import { ICommandPalette } from '@jupyterlab/apputils';
+
 /**
  * Initialization data for the renkulab-menu-extension extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'renkulab-menu-extension',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension renkulab-menu-extension is activated!');
+  requires: [ICommandPalette, IMainMenu],
+  activate: (
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    mainMenu: IMainMenu
+  ) => {
+    console.log('Renku Extension Activation!')
+    const { commands } = app;
+
+    // Add a command
+    const command = 'renku-tools:renkulab-menu-extension';
+    commands.addCommand(command, {
+      label: 'Execute renku-tools:renkulab-menu-extension Command',
+      caption: 'Execute renku-tools:renkulab-menu-extension Command',
+      execute: (args: any) => {
+        console.log(
+          `renku-tools:renkulab-menu-extension has been called ${args['origin']}.`
+        );
+        window.alert(
+          `renku-tools:renkulab-menu-extension has been called ${args['origin']}.`
+        );
+      }
+    });
+
+    // Add the command to the command palette
+    const category = 'Renku Tools';
+    palette.addItem({
+      command,
+      category,
+      args: { origin: 'from the palette' }
+    });
+
+    // Create a menu
+    const tutorialMenu: Menu = new Menu({ commands });
+    tutorialMenu.title.label = 'Renku Tools Menu';
+    mainMenu.addMenu(tutorialMenu, { rank: 80 });
+
+    // Add the command to the menu
+    tutorialMenu.addItem({ command, args: { origin: 'from the menu' } });
   }
 };
 
