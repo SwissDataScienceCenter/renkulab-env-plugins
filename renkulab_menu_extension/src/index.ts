fe79de3d22079a275ea0ead2460065256c725b59
Emma Jablonski <e.jablonski@gmail.com>
2020-04-21 09:58:24 +0200

    build: added "open renkulab.io" window to command

--- renkulab_menu_extension/src/index.ts
+++ renkulab_menu_extension/src/index.ts
@@ -24,18 +24,17 @@ const extension: JupyterFrontEndPlugin<void> = {
     console.log('Renku Extension Activation!')
     const { commands } = app;
 
-    // Add a command
+    // Add a command to open renkulab.io
     const command = 'renku-tools:renkulab-menu-extension';
     commands.addCommand(command, {
-      label: 'Execute renku-tools:renkulab-menu-extension Command',
+      label: 'Open renkulab.io',
       caption: 'Execute renku-tools:renkulab-menu-extension Command',
       execute: (args: any) => {
+
         console.log(
           `renku-tools:renkulab-menu-extension has been called ${args['origin']}.`
         );
-        window.alert(
-          `renku-tools:renkulab-menu-extension has been called ${args['origin']}.`
-        );
+        window.open("https://renkulab.io")
       }
     });
 
@@ -47,13 +46,15 @@ const extension: JupyterFrontEndPlugin<void> = {
       args: { origin: 'from the palette' }
     });
 
+
     // Create a menu
-    const tutorialMenu: Menu = new Menu({ commands });
-    tutorialMenu.title.label = 'Renku Tools Menu';
-    mainMenu.addMenu(tutorialMenu, { rank: 80 });
+    const renkuMenu: Menu = new Menu({ commands });
+    renkuMenu.title.label = 'Renku Tools Menu';
+    mainMenu.addMenu(renkuMenu, { rank: 80 });
 
     // Add the command to the menu
-    tutorialMenu.addItem({ command, args: { origin: 'from the menu' } });
+    renkuMenu.addItem({ command, args: { origin: 'from the menu' } });
+
   }
 };
 
