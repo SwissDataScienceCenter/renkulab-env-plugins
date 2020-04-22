318155d70b008141bfdbae345ca04a31e1e8bd39
Emma Jablonski <e.jablonski@gmail.com>
2020-04-22 07:00:24 +0200

    build: add link to /tree and /rstudio

--- renkulab_menu_extension/src/index.ts
+++ renkulab_menu_extension/src/index.ts
@@ -3,6 +3,8 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { PageConfig } from '@jupyterlab/coreutils';
+
 import { IMainMenu } from '@jupyterlab/mainmenu';
 
 import { Menu } from '@lumino/widgets';
@@ -20,11 +22,12 @@ const extension: JupyterFrontEndPlugin<void> = {
     app: JupyterFrontEnd,
     palette: ICommandPalette,
     mainMenu: IMainMenu
+
   ) => {
     console.log('Renku Extension Activation!')
     const { commands } = app;
 
-    // 1. Add a command to open renkulab.io
+    // 1. Add a command
     const back_to_renkulab = 'renku-tools:back_to_renkulab';
     commands.addCommand(back_to_renkulab, {
       label: 'Open renkulab.io',
@@ -34,7 +37,7 @@ const extension: JupyterFrontEndPlugin<void> = {
         console.log(
           `renku-tools:back-to-renkulab has been called ${args['origin']}.`
         );
-        window.open("https://renkulab.io")
+        window.open('https://renkulab.io')
       }
     });
 
@@ -47,7 +50,7 @@ const extension: JupyterFrontEndPlugin<void> = {
         console.log(
           `renku-tools:renkulab-docs has been called ${args['origin']}.`
         );
-        window.open("https://renku.readthedocs.io/en/latest/")
+        window.open('https://renku.readthedocs.io/en/latest/')
       }
     });
 
@@ -60,9 +63,45 @@ const extension: JupyterFrontEndPlugin<void> = {
         console.log(
           `renku-tools:renku-python-docs has been called ${args['origin']}.`
         );
-        window.open("https://renku-python.readthedocs.io/en/latest/")
+        window.open('https://renku-python.readthedocs.io/en/latest/')
       }
     });
+
+    const open_classic_notebook = 'renku-tools:open-classic-notebook';
+    commands.addCommand(open_classic_notebook, {
+      label: 'Open /tree endpoint',
+      caption: 'Execute renku-tools:open-classic-notebook Command',
+      execute: (args: any) => {
+
+        console.log(
+          `renku-tools:open-classic-notebook has been called ${args['origin']}.`
+        );
+
+        // thanks to jupyterlab's help menu code here:
+        // https://github.com/jupyterlab/jupyterlab/blob/f0153e0258b32674c9aec106383ddf7b618cebab/packages/help-extension/src/index.tsx
+        window.open(PageConfig.getBaseUrl() + 'tree');
+
+      }
+    });
+
+    // TODO: Conditionally gray this out this based on first line in docker image
+    const open_rstudio = 'renku-tools:open-rstudio';
+    commands.addCommand(open_rstudio, {
+      label: 'Open /tree endpoint',
+      caption: 'Execute renku-tools:open-rstudio Command',
+      execute: (args: any) => {
+
+        console.log(
+          `renku-tools:open-rstudio has been called ${args['origin']}.`
+        );
+
+        // thanks to jupyterlab's help menu code here:
+        // https://github.com/jupyterlab/jupyterlab/blob/f0153e0258b32674c9aec106383ddf7b618cebab/packages/help-extension/src/index.tsx
+        window.open(PageConfig.getBaseUrl() + 'rstudio');
+
+      }
+    });
+
 
     // 2. Add the command to the command palette
     const category = 'Renku Tools';
@@ -84,6 +123,18 @@ const extension: JupyterFrontEndPlugin<void> = {
       args: { origin: 'from the palette' }
     });
 
+    palette.addItem({
+      command:open_classic_notebook,
+      category,
+      args: { origin: 'from the palette' }
+    });
+
+    palette.addItem({
+      command:open_rstudio,
+      category,
+      args: { origin: 'from the palette' }
+    });
+
 
     // 3. Create a menu (just once)
     const renkuMenu: Menu = new Menu({ commands });
@@ -96,6 +147,10 @@ const extension: JupyterFrontEndPlugin<void> = {
     renkuMenu.addItem({ command:renkulab_docs, args: { origin: 'from the menu' } });
 
     renkuMenu.addItem({ command:renku_python_docs, args: { origin: 'from the menu' } });
+
+    renkuMenu.addItem({ command:open_classic_notebook, args: { origin: 'from the menu' } });
+
+    renkuMenu.addItem({ command:open_rstudio, args: { origin: 'from the menu' } });
 
   }
 };
